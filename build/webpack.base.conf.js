76c34a38ac79e617bc68a2ad0f438500341116de
cying <940508064@qq.com>
2018-08-24 18:03:58 +0800

    为webpack配置添加注释

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -1,13 +1,16 @@
+
 'use strict'
 const path = require('path')
 const utils = require('./utils')
 const config = require('../config')
 const vueLoaderConfig = require('./vue-loader.conf')
 
+// 获取绝对路径
 function resolve (dir) {
   return path.join(__dirname, '..', dir)
 }
 
+// 定义一下代码检测的规则
 const createLintingRule = () => ({
   test: /\.(js|vue)$/,
   loader: 'eslint-loader',
@@ -20,10 +23,13 @@ const createLintingRule = () => ({
 })
 
 module.exports = {
+  // 基础上下文
   context: path.resolve(__dirname, '../'),
+  // webpack的入口文件
   entry: {
     app: './src/main.js'
   },
+  // webpack的输出文件
   output: {
     path: config.build.assetsRoot,
     filename: '[name].js',
@@ -31,43 +37,55 @@ module.exports = {
       ? config.build.assetsPublicPath
       : config.dev.assetsPublicPath
   },
+  /**
+   * 当webpack试图去加载模块的时候，它默认是查找以 .js结尾的文件的，
+   * 它并不知道 .vue结尾的文件是什么鬼玩意儿，
+   * 所以我们要在配置文件中告诉webpack
+   * 遇到 .vue结尾的文件也要去加载
+   * 添加 resolve 配置项，如下:
+   */
   resolve: {
     extensions: ['.js', '.vue', '.json'],
-    alias: {
+    alias: {  // 创建别名
       'vue$': 'vue/dist/vue.esm.js',
-      '@': resolve('src'),
+      '@': resolve('src'),  // 如 '@/components/HelloWorld'
     }
   },
+  // 不同类型模块的处理规则 就是用不同的loader处理不同的文件
   module: {
     rules: [
       ...(config.dev.useEslint ? [createLintingRule()] : []),
-      {
+      { // 对所有 .vue文件使用vue-loader进行编译
         test: /\.vue$/,
         loader: 'vue-loader',
         options: vueLoaderConfig
       },
-      {
+      { // 对src喝test文件夹下的.js文件使用babel-loader将es6+的代码转成es5
         test: /\.js$/,
         loader: 'babel-loader',
         include: [resolve('src'), resolve('test'), resolve('node_modules/webpack-dev-server/client')]
       },
-      {
+      { // 对图片资源文件使用url-loader
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
         loader: 'url-loader',
         options: {
+          // 小于10k的图片转成base64编码的dataURL字符串写到代码中
           limit: 10000,
+          // 其他的图片转移到静态资源文件夹
           name: utils.assetsPath('img/[name].[hash:7].[ext]')
         }
       },
-      {
+      { // 对多媒体资源文件使用url-loader
         test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
         loader: 'url-loader',
         options: {
+          // 小于10k的图片转成base64编码的dataURL字符串写到代码中
           limit: 10000,
+          // 其他的图片转移到静态资源文件夹
           name: utils.assetsPath('media/[name].[hash:7].[ext]')
         }
       },
-      {
+      { // 对字体资源文件使用url-loader
         test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
         loader: 'url-loader',
         options: {
